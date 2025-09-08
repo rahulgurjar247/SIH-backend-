76bf7235669ce4bc3b4dcf3223bf91ba8247ee24
Rahul gurjar <rahulgurjar824582@gmail.com>
2025-09-08 13:21:08 +0530

    system

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // preflight
+// app.options("*", cors(corsOptions)); // preflight
 
 // ----------------- Body Parsing -----------------
 app.use(express.json({ limit: "10mb" }));
